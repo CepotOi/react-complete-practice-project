cc6be7e84daac257fd02bb26d4a35e240e0ce83f
CepotOi <rizkihutama69@gmail.com>
2022-05-21 10:55:26 +0700

    changging from useState to useRef in FormUser

--- src/components/Users/FormUser/FormUser.jsx
+++ src/components/Users/FormUser/FormUser.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Button from '../../UI/Button/Button';
 import Card from '../../UI/Card/Card';
 import Modal from '../../UI/Modal/Modal';
 import styles from './FormUser.module.css';
 
 const FormUser = props => {
-  const INITIAL_STATE = {
-    id: '',
-    name: '',
-    age: ''
-  };
+  // const INITIAL_STATE = {
+  //   id: '',
+  //   name: '',
+  //   age: ''
+  // };
 
-  const [addUser, setAddUser] = useState(INITIAL_STATE);
+  // const [addUser, setAddUser] = useState(INITIAL_STATE);
+  const inputRef = useRef({});
   const [error, setError] = useState(null);
 
-  const resetState = () => {
-    setAddUser(INITIAL_STATE);
+  const resetInput = () => {
+    //* using state
+    // setAddUser(INITIAL_STATE);
+
+    //* using ref
+    inputRef.current.name.value = '';
+    inputRef.current.age.value = '';
   };
 
-  const handleInputChange = event => {
-    const { name, value } = event.target;
-    setAddUser(prevState => ({
-      ...prevState,
-      [name]: value.trim()
-    }));
-  };
+  // const handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   setAddUser(prevState => ({
+  //     ...prevState,
+  //     [name]: value.trim()
+  //   }));
+  // };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    const userName = addUser.name;
-    const userAge = addUser.age;
+
+    //* using state
+    // const userName = addUser.name;
+    // const userAge = addUser.age;
+
+    //* using ref
+    const userName = inputRef.current.name.value;
+    const userAge = inputRef.current.age.value;
 
     if (userName.trim().length === 0 || userAge === '') {
       setError({ title: 'Invalid Input', message: 'Please fill out all fields' });
@@ -43,12 +55,12 @@ const FormUser = props => {
 
     const userInput = {
       id: Math.round((Math.random() * 100) + Date.now()).toString(),
-      name: addUser.name,
-      age: parseInt(addUser.age)
+      name: userName,
+      age: parseInt(userAge)
     };
 
     props.onAddUser(userInput);
-    return resetState();
+    return resetInput();
   };
 
   return (
@@ -61,11 +73,25 @@ const FormUser = props => {
         <form onSubmit={formSubmitHandler}>
           <div className={styles['form-control']}>
             <label htmlFor="__user_name">User Name</label>
-            <input type="text" id="__user_name" name="name" value={addUser.name} onChange={handleInputChange} />
+            <input
+              type="text"
+              id="__user_name"
+              name="name"
+              // value={addUser.name}
+              // onChange={handleInputChange}
+              ref={ref => inputRef.current['name'] = ref}
+            />
           </div>
           <div className={styles['form-control']}>
             <label htmlFor="__user_age">Age (Years)</label>
-            <input type="number" id="__user_age" name="age" value={addUser.age} onChange={handleInputChange} />
+            <input
+              type="number"
+              id="__user_age"
+              name="age"
+              // value={addUser.age}
+              // onChange={handleInputChange}
+              ref={ref => inputRef.current['age'] = ref}
+            />
           </div>
           <div className={styles['form-control__actions']}>
             <Button type="submit">Add User</Button>
